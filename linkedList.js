03df09d07daaa143dfe9fbdfae0d395e05907b5f
Farooq Mahmud <farooqm@techniqly.com>
2018-05-19 02:18:51 -0700

    Added linked list code.

--- linkedList.js
+++ linkedList.js
@@ -1,80 +1,235 @@
-var LinkedListNode = (function () {
-
-    function LinkedListNode(val) {
-        this._val = val;
-        this._next = null;
-    }
-
-    LinkedListNode.prototype.getVal = function () {
-        return this._val;
-    }
-
-    LinkedListNode.prototype.getNext = function () {
-        return this._next;
-    }
-
-    LinkedListNode.prototype.setNext = function (node) {
-        this._next = node;
-    }
-
-    return LinkedListNode;
-})();
-
 'use strict';
 
-var LinkedList = (function () {
+function LinkedListNode(val) {
+    this._val = val;
+    this._next = null;
+}
 
-    function LinkedList() {
-        this._length = 0;
-    }
+LinkedListNode.prototype = (function () {
 
-    LinkedList.prototype._incrementLength = function () {
-        this._length += 1;
-    }
+    return {
+        constructor: LinkedListNode,
 
-    LinkedList.prototype.add = function (val) {
-        var newNode = new LinkedListNode(val);
+        getVal: function () {
+            return this._val;
+        },
 
-        if (this.getLength() === 0) {
-            this._head = newNode;
+        setVal: function (val) {
+            this._val = val;
+        },
+
+        getNext: function () {
+            return this._next;
+        },
+
+        setNext: function (node) {
+            this._next = node;
         }
-        else {
-            var currentNode = this.getHead();
 
-            while (currentNode.getNext() != null) {
+    }
+})();
+
+function LinkedList() {
+
+    this._head = null;
+    this._length = 0;
+
+    this._incrementLength = function () {
+        this._length += 1;
+    };
+
+    this._decrementLength = function () {
+        this._length -= 1;
+    };
+
+    this._getHead = function () {
+        return this._head;
+    };
+}
+
+LinkedList.prototype = (function () {
+
+    return {
+        constructor: LinkedList,
+
+        push: function (val) {
+            var newNode = new LinkedListNode(val);
+
+            if (this.getLength() === 0) {
+                this._head = newNode;
+            }
+            else {
+                var currentNode = this._getHead();
+
+                while (currentNode.getNext() != null) {
+                    currentNode = currentNode.getNext();
+                }
+
+                currentNode.setNext(newNode);
+            }
+
+            this._incrementLength();
+        },
+
+        pop: function () {
+            if (this.getLength() === 0) {
+                throw {
+                    'name': 'InvalidOperation',
+                    'message': 'Cannot call this function on an empty list.'
+                }
+            }
+
+            var poppedNode = this.getAt(this.getLength() - 1);;
+
+            if (this.getLength() === 1) {
+                this._head = null;
+            }
+            else {
+                var newLastNode = this.getAt(this.getLength() - 2);
+                newLastNode.setNext(null);
+            }
+
+            this._decrementLength();
+
+            return poppedNode;
+        },
+
+        getLength: function () {
+            return this._length;
+        },
+
+        getAt: function (index) {
+            if (this.getLength() === 0) {
+                throw {
+                    'name': 'InvalidOperation',
+                    'message': 'Cannot call this function on an empty list.'
+                }
+            }
+
+            if (index > this.getLength() - 1 || index < 0) {
+                throw {
+                    'name': 'ArgumentOutOfRange',
+                    'message': 'Index is out of range.'
+                }
+            }
+
+            var currentNode = this._getHead();
+
+            for (var i = 0; i < index; i++) {
                 currentNode = currentNode.getNext();
             }
 
-            currentNode.setNext(newNode);
-        }
+            return currentNode;
+        },
 
-        this._incrementLength();
-    };
-    LinkedList.prototype.getLength = function() {
-        return this._length;
+        reverse: function (index) {
+            if (this.getLength() === 0) {
+                throw {
+                    'name': 'InvalidOperation',
+                    'message': 'Cannot call this function on an empty list.'
+                }
+            }
+
+            if (this.getLength() === 1) {
+                return this._getHead();
+            }
+
+            var headIndex = 0;
+            var tailIndex = this.getLength() - 1;
+
+            while (headIndex < tailIndex) {
+                var currentHeadNode = this.getAt(headIndex);
+                var currentTailNode = this.getAt(tailIndex);
+                var tempVal = currentHeadNode.getVal();
+
+                currentHeadNode.setVal(currentTailNode.getVal());
+                currentTailNode.setVal(tempVal);
+                headIndex++;
+                tailIndex--;
+            }
+        },
+
+        insertAt: function (index, val) {
+
+            if(index < 0) {
+                throw {
+                    'name': 'ArgumentOutOfRange',
+                    'message': 'Index is out of range.'
+                }
+            }
+
+            if (this.getLength() === 0 && index > 0) {
+                throw {
+                    'name': 'ArgumentOutOfRange',
+                    'message': 'Index is out of range.'
+                }
+            }
+
+            if (this.getLength() > 0 && index > this.getLength() - 1) {
+                throw {
+                    'name': 'ArgumentOutOfRange',
+                    'message': 'Index is out of range.'
+                }
+            }
+
+            if (this.getLength() === 0) {
+                this.push(val);
+                return;
+            }
+
+            var indexNodeBefore = index - 1;
+
+            if (indexNodeBefore < 0) {
+                var tempNode = this._head;
+                this._head = new LinkedListNode(val);
+                this._head.setNext(tempNode);
+                this._incrementLength();
+                return;
+            }
+            else {
+                var nodeBefore = this.getAt(indexNodeBefore);
+                var tempNode = nodeBefore.getNext();
+                var newNode = new LinkedListNode(val);
+                newNode.setNext(tempNode);
+                nodeBefore.setNext(newNode);
+            }
+
+            this._incrementLength();
+        },
+
+        removeAt: function(index) {
+
+            if(this.getLength() === 0) {
+                throw {
+                    'name': 'InvalidOperation',
+                    'message': 'Cannot call this function on an empty list.'
+                }
+            }
+
+            if (index < 0) {
+                throw {
+                    'name': 'ArgumentOutOfRange',
+                    'message': 'Index is out of range.'
+                }
+            }
+
+            if (index > this.getLength() - 1) {
+                throw {
+                    'name': 'ArgumentOutOfRange',
+                    'message': 'Index is out of range.'
+                }
+            }
+
+            if(this.getLength() === 1 || index === this.getLength() - 1){
+                return this.pop();
+            }
+
+            var indexNodeBefore = index - 1;
+            var nodeBefore = this.getAt(indexNodeBefore);
+            var nodeToDelete = nodeBefore.getNext();
+            nodeBefore.setNext(nodeToDelete.getNext());
+            this._decrementLength();
+            return nodeToDelete;
+        }
     }
-    
-    LinkedList.prototype.getHead = function () {
-        return this._head;
-    }
-
-    LinkedList.prototype.getAt = function (index) {
-        if (this.getLength() === 0) {
-            throw 'Cannot call this function on an empty list.';
-        }
-
-        if (index > this.getLength() - 1) {
-            throw 'Index is out of range.'
-        }
-
-        var currentNode = this.getHead();
-
-        for (var i = 0; i < index; i++){
-            currentNode = currentNode.getNext();
-        }
-
-        return currentNode;
-    }
-
-    return LinkedList;
 })();
--- linkedList.spec.js
+++ linkedList.spec.js
@@ -1,45 +1,354 @@
 'use strict';
 
-describe('LinkedListNode Tests', function() {
+describe('LinkedListNode Tests', function () {
 
-    it('Returns the val property value', function() {
+    it('Returns the val property value', function () {
         var vals = [10, "hello", false];
 
-        for(var i = 0; i < vals.length; i++){
+        for (var i = 0; i < vals.length; i++) {
             var node = new LinkedListNode(vals[i]);
             expect(node.getVal()).toBe(vals[i]);
         }
-        
+
     });
 });
 
-describe('LinkedList Tests', function() {
+describe('LinkedList Tests', function () {
 
     var list = null;
 
-    beforeEach(function() {
+    beforeEach(function () {
         list = new LinkedList();
     });
 
-    it('Adds the first node at the head of the list.', function() {
-        var expectedValue = "bubba";
-        list.add(expectedValue);
-        expect(list.getHead().getVal()).toBe(expectedValue);
-        expect(list.getHead().getNext()).toBeNull();
-        expect(list.getLength()).toBe(1);
+    describe('push() Tests', function () {
+
+        it('Adds the first node at the head of the list.', function () {
+            var expectedValue = "bubba";
+            list.push(expectedValue);
+            expect(list.getAt(0).getVal()).toBe(expectedValue);
+            expect(list.getAt(0).getNext()).toBeNull();
+        });
+
+        it('Adds multiple nodes to the list', function () {
+            var inputs = [10, 20, 30];
+
+            inputs.forEach(function (val) {
+                list.push(val);
+            });
+
+            for (var i = 0; i < inputs.length; i++) {
+                expect(list.getAt(i).getVal()).toBe(inputs[i]);
+            }
+        });
     });
 
-    it('Adds multiple nodes to the list', function() {
-        var inputs = [10, 20, 30];
 
-        for(var i = 0; i < inputs.length; i++) {
-            list.add(inputs[i]);
-        }
+    describe('pop() Tests', function () {
 
-        for(var i = 0; i < inputs.length; i++) {
-            expect(list.getAt(i).getVal()).toBe(inputs[i]);
-        }
-        
-        expect(list.getLength()).toBe(3);
+        it('Should result in an empty list when called on a one node list', function () {
+           list.push(10);
+           var node = list.pop();
+           
+            expect(list.getLength()).toBe(0);
+            expect(node.getVal()).toBe(10);
+        });
+
+        it('Throws error for an empty list', function () {
+            try {
+                list.pop();
+                throw Error("Did not throw the expected exception.")                                
+            }
+            catch (ex) {
+                expect(ex.name).toBe('InvalidOperation');
+            }
+
+        });
+
+        it('Removes the last node from a multi-node list', function() {
+            var inputs = [10, 20, 30, 40, 50];
+
+            inputs.forEach(function (val) {
+                list.push(val);
+            });
+
+            for(var i = 0; i < list.getLength(); i++) {
+                var poppedNode = list.pop();
+                expect(poppedNode.getVal()).toBe(inputs[inputs.length - i - 1]);
+                expect(list.getLength()).toBe(inputs.length - i - 1)
+            }
+            
+
+        });
+
+    });
+
+
+    describe('getLength() Tests', function () {
+
+        it('Returns zero for an empty list', function () {
+            expect(list.getLength()).toBe(0);
+        });
+
+        it('Returns the list length', function () {
+            var inputs = [10, 20, 30];
+
+            inputs.forEach(function (val) {
+                list.push(val);
+            });
+
+            expect(list.getLength()).toBe(inputs.length);
+        });
+    });
+
+    describe('getAt() Tests', function () {
+
+        it('Returns the head node when given 0 index', function () {
+            list.push("foo");
+            expect(list.getAt(0).getVal()).toBe("foo");
+        });
+
+        it('Returns the node at the given index', function () {
+            var inputs = [10, 20, 30];
+
+            inputs.forEach(function (val) {
+                list.push(val);
+            });
+
+            for (var i = 0; i < inputs.length; i++) {
+                expect(list.getAt(i).getVal()).toBe(inputs[i]);
+            }
+        });
+
+        it('Throws error for an empty list', function () {
+            try {
+                list.getAt(0);
+                throw Error("Did not throw the expected exception.")                                
+            }
+            catch (ex) {
+                expect(ex.name).toBe('InvalidOperation');
+            }
+
+        });
+
+        it('Throws error when index is greater than list length', function () {
+            var inputs = [10, 20, 30];
+
+            inputs.forEach(function (val) {
+                list.push(val);
+            });
+
+            try {
+                list.getAt(4);
+                throw Error("Did not throw the expected exception.")                                
+            }
+            catch (ex) {
+                expect(ex.name).toBe('ArgumentOutOfRange');
+            }
+        });
+
+        it('Throws error when index is negative', function () {
+            var inputs = [10, 20, 30];
+
+            inputs.forEach(function (val) {
+                list.push(val);
+            });
+
+            try {
+                list.getAt(-1);
+                throw Error("Did not throw the expected exception.")                                
+            }
+            catch (ex) {
+                expect(ex.name).toBe('ArgumentOutOfRange');
+            }
+        });
+    });
+
+    describe('reverse() tests', function() {
+
+        it('Throws error when list is empty', function() {
+
+            try {
+                list.reverse();
+                throw Error("Did not throw the expected exception.")                                
+            }
+            catch (ex) {
+                expect(ex.name).toBe('InvalidOperation');
+            }
+        });
+
+        it('Should reverse a single node list', function() {
+            list.push(10);
+            list.reverse();
+            expect(list.getAt(0).getVal()).toBe(10);
+        });
+
+        it('Should reverse list with even number of nodes', function() {
+            var inputs = [10, 20, 30, 40];
+
+            inputs.forEach(function (val) {
+                list.push(val);
+            });
+
+            list.reverse();
+
+            for (var i = 0; i < inputs.length; i++) {
+                expect(list.getAt(i).getVal()).toBe(inputs[inputs.length - 1 - i]);
+            }
+        });
+
+        it('Should reverse list with odd number of nodes', function() {
+            var inputs = [10, 20, 30, 40, 50];
+
+            inputs.forEach(function (val) {
+                list.push(val);
+            });
+
+            list.reverse();
+
+            for (var i = 0; i < inputs.length; i++) {
+                expect(list.getAt(i).getVal()).toBe(inputs[inputs.length - 1 - i]);
+            }
+        });
+    });
+
+    describe('insertAt() Tests', function() {
+
+        it('Should throw when index is greater than list length', function() {
+            try {
+                list.insertAt(1);
+                throw Error("Did not throw the expected exception.")                                
+            }
+            catch (ex) {
+                expect(ex.name).toBe('ArgumentOutOfRange');
+            }
+        });
+
+        it('Should throw when index is greater than list length', function() {
+            try {
+                list.push(1);
+                list.push(2);
+                list.insertAt(2);
+                throw Error("Did not throw the expected exception.")                                
+            }
+            catch (ex) {
+                expect(ex.name).toBe('ArgumentOutOfRange');
+            }
+        });
+
+        it('Should throw when index is negative', function() {
+            try {
+                list.insertAt(-1);
+                throw Error("Did not throw the expected exception.")                
+            }
+            catch (ex) {
+                expect(ex.name).toBe('ArgumentOutOfRange');
+            }
+        });
+
+        it('Should insert a new node when list is empty', function() {
+            list.insertAt(0, "foo");
+            expect(list.getLength()).toBe(1);
+            expect(list.getAt(0).getVal()).toBe("foo");
+        });
+
+        it('Should insert a new node at the head of the list', function() {
+            var inputs = ["foo", "bar", "baz"];
+
+            inputs.forEach(function(val) {
+                list.push(val);
+            });
+
+            list.insertAt(0, "bah");
+            expect(list.getLength()).toBe(4);
+            expect(list.getAt(0).getVal()).toBe("bah");            
+            expect(list.getAt(1).getVal()).toBe("foo");
+        });
+
+        it('Should insert a new node before the node at index', function() {
+            var inputs = ["foo", "bar", "baz"];
+
+            inputs.forEach(function(val) {
+                list.push(val);
+            });
+
+            list.insertAt(2, "bah");
+            expect(list.getLength()).toBe(4);
+            expect(list.getAt(2).getVal()).toBe("bah");            
+            expect(list.getAt(3).getVal()).toBe("baz");
+        });
+    });
+
+    describe('removeAt() Tests', function() {
+
+        it('Should throw on an empty list', function() {
+            try {
+                list.removeAt(0);
+                throw Error("Did not throw the expected exception.")                
+            }
+            catch (ex) {
+                expect(ex.name).toBe('InvalidOperation');
+            }
+        });
+
+        it('Should throw when index is greater than list length', function() {
+            try {
+                list.push(100);
+                list.removeAt(1);
+                throw Error("Did not throw the expected exception.")
+            }
+            catch (ex) {
+                expect(ex.name).toBe('ArgumentOutOfRange');
+            }
+        });
+
+        it('Should throw when index is negative', function() {
+            try {
+                list.push("foo");
+                list.removeAt(-1);
+                throw Error("Did not throw the expected exception.")                
+            }
+            catch (ex) {
+                expect(ex.name).toBe('ArgumentOutOfRange');
+            }
+        });
+
+        it('Should result in an empty list when called on a one node list', function() {
+            list.push("foo");
+            var removedNode = list.removeAt(0);
+
+            expect(list.getLength()).toBe(0);
+            expect(removedNode.getVal()).toBe("foo");
+        });
+
+        it('Should remove the last node', function() {
+            var inputs = ["foo", "bar", "baz", "bah"];
+
+            inputs.forEach(function(val) {
+                list.push(val);
+            });
+
+            var removedNode = list.removeAt(3);
+            expect(list.getLength()).toBe(3);
+            expect(removedNode.getVal()).toBe("bah");
+            expect(list.getAt(2).getVal()).toBe("baz");
+            expect(list.getAt(2).getNext()).toBeNull();
+        })
+
+        it('Should remove the node at index', function() {
+            
+            var inputs = ["foo", "bar", "baz", "bah"];
+
+            inputs.forEach(function(val) {
+                list.push(val);
+            });
+
+            var removedNode = list.removeAt(2);
+
+            expect(list.getLength()).toBe(3);
+            expect(removedNode.getVal()).toBe("baz");
+            expect(list.getAt(2).getVal()).toBe("bah");
+            expect(list.getAt(1).getVal()).toBe("bar");
+        });
     });
 });
